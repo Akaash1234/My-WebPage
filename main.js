24b09419d28b9092418d353c2cfb50c6b474ea8a
Akaash1234 <68061991+Akaash1234@users.noreply.github.com>
2021-01-12 20:35:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function changeimage() {
+    document.getElementById("image1").src = "https://cdn.mos.cms.futurecdn.net/42E9as7NaTaAi4A6JcuFwG-1200-80.jpg";
+    document.getElementById("para").innerHTML = "I love BanAna ToO"
+}
